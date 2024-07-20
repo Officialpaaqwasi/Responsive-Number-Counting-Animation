44bbe3b17222fc1c769b83cd10292d4fde4035ec
officialpaaqwasi <iklutse@st.ug.edu.gh>
2024-07-20 07:41:01 +0000

    20/07/24 phase2

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ let interval = 5000;
 valueDisplays.forEach((valueDisplays) => {
  let startValue = 0;
  let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+ let duration = Math.floor(interval / endValue);
+ let counter = setInterval(function(){
+  startValue += 1;
+  valueDisplays.textContent = startValue;
+  if(startValue == endValue){
+   clearInterval(counter);
+  }
+ }, duration);
 });
