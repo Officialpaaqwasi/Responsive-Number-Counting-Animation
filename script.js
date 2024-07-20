4acacd19a3f6c48aa6645fcc23f9477f6a0f75f5
officialpaaqwasi <iklutse@st.ug.edu.gh>
2024-07-20 06:25:30 +0000

    20/07/24 phase1

---
+++ script.js
@@ -0,0 +1,8 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+
+valueDisplays.forEach((valueDisplays) => {
+ let startValue = 0;
+ let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+});
